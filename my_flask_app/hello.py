0f1b96684228cd96417fb434efbf5ab61dd90d3d
dutta712 <67039065+dutta712@users.noreply.github.com>
2021-04-20 12:32:53 +0530

    Update hello.py

--- my_flask_app/hello.py
+++ my_flask_app/hello.py
@@ -1,15 +1,7 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-db =SQLAlchemy(app)
-
-class Users(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(120), nullable= False)
-    email = db.Column(db.String(120), nullable= False, unique = True)
 @app.route('/')
 def index():
     return render_template('index.html')
